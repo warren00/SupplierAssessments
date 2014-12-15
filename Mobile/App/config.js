0e9c8489ce62f7eeefa604d8ee0f34a3695b8226
Steve Harper <steveharper886@gmail.com>
2014-12-15 12:49:41 +0000

    Updated search suppliers navigation bar text

--- Mobile/App/config.js
+++ Mobile/App/config.js
@@ -13,8 +13,8 @@
                           { route: "assessment/:id*details", moduleId: "viewmodels/assessmentDetail", title: "Assessment Detail", hash: "#assessment/:id", nav: false },
                           { route: "deliveryAssessment/:id*details", moduleId: "viewmodels/deliveryAssessment", title: "Delivery Assessment", nav: false }];
 
-    routes['Administrator'] = [{ route: "", title: "Search Suppliers", moduleId: "viewmodels/searchSuppliers", name: "Search", nav: true },
-                               { route: "dashboard/:id", title: "Dashboard", moduleId: "viewmodels/dashboard", name: "Dashboard", nav: true },
+    routes['Administrator'] = [{ route: "", title: "Suppliers", moduleId: "viewmodels/searchSuppliers", name: "Search", nav: true },
+                               { route: "dashboard/:id", title: "Dashboard", moduleId: "viewmodels/dashboard", name: "Dashboard", nav: false },
                                { route: "assessments/:id", moduleId: "viewmodels/assessments", name: "Assessments", hash: "#assessments/:id", nav: false, },
                                { route: "assessment/:id*details", moduleId: "viewmodels/assessmentDetail", title: "Assessment Detail", hash: "#assessment/:id", nav: false },
                                { route: "deliveryAssessment/:id*details", moduleId: "viewmodels/deliveryAssessment", title: "Delivery Assessment", nav: false }];
