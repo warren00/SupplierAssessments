d5c85bbbebd141cf1f55b95a56486097427501f6
Steve Harper <steveharper886@gmail.com>
2014-12-16 16:53:24 +0000

    Added code to initialize durandal from device ready on mobile.

--- App/main.js
+++ App/main.js
@@ -10,24 +10,35 @@
 define('jquery', function () { return jQuery; });
 define('knockout', ko);
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
-    //>>excludeStart("build", true);
-    system.debug(true);
-    //>>excludeEnd("build");
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (system, app, viewLocator) {
 
-    app.title = 'Supplier Assessment';
+    var useragent = navigator.userAgent.toLowerCase();
+    if (useragent.match(/android/) || useragent.match(/iphone/) || useragent.match(/ipad/) || useragent.match('ios') || useragent.match('Windows Phone') || useragent.match('iemobile')) {
+        document.addEventListener('deviceready', onDeviceReady, false);
+    }
+    else {
+        onDeviceReady();
+    }
 
-    app.configurePlugins({
-        router: true,
-        dialog: true
-    });
+    function onDeviceReady() {
+        //>>excludeStart("build", true);
+        system.debug(true);
+        //>>excludeEnd("build");
 
-    app.start().then(function() {
-        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-        //Look for partial views in a 'views' folder in the root.
-        viewLocator.useConvention();
+        app.title = 'Supplier Assessment';
 
-        //Show the app by setting the root view model for our application with a transition.
-        app.setRoot('viewmodels/shell', 'entrance');
-    });
+        app.configurePlugins({
+            router: true,
+            dialog: true
+        });
+
+        app.start().then(function () {
+            //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+            //Look for partial views in a 'views' folder in the root.
+            viewLocator.useConvention();
+
+            //Show the app by setting the root view model for our application with a transition.
+            app.setRoot('viewmodels/shell', 'entrance');
+        });
+    };
 });
