821959a602fef1415df0046e247493181bf41b93
Steve Harper <steveharper886@gmail.com>
2014-12-17 14:41:20 +0000

    Fix

--- App/viewmodels/shell.js
+++ App/viewmodels/shell.js
@@ -56,16 +56,6 @@
         function checkConnection() {
             var networkState = navigator.connection.type;
 
-            var states = {};
-            states[Connection.UNKNOWN] = 'Unknown connection';
-            states[Connection.ETHERNET] = 'Ethernet connection';
-            states[Connection.WIFI] = 'WiFi connection';
-            states[Connection.CELL_2G] = 'Cell 2G connection';
-            states[Connection.CELL_3G] = 'Cell 3G connection';
-            states[Connection.CELL_4G] = 'Cell 4G connection';
-            states[Connection.CELL] = 'Cell generic connection';
-            states[Connection.NONE] = 'No network connection';
-
             if (networkState == Connection.NONE) {
                 window.navigator.notification.alert("No data connection", function () {
                     document.location = "login.html"
@@ -93,6 +83,8 @@
             }).fail(function (result) {
                 if (result.status == 401)
                     document.location = "./login.html";
+
+                throw result;
             }));
         }
 
