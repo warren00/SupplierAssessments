a52a47d86a8b7a33f21069db97fbbb7a78966b04
Steve Harper <steveharper886@gmail.com>
2014-12-17 08:39:28 +0000

    Fix

--- App/main.js
+++ App/main.js
@@ -10,35 +10,24 @@
 define('jquery', function () { return jQuery; });
 define('knockout', ko);
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (system, app, viewLocator) {
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
 
-    var useragent = navigator.userAgent.toLowerCase();
-    if (useragent.match(/android/) || useragent.match(/iphone/) || useragent.match(/ipad/) || useragent.match('ios') || useragent.match('Windows Phone') || useragent.match('iemobile')) {
-        setTimeout(document.addEventListener('deviceready', onDeviceReady, false), 1000);
-    }
-    else {
-        onDeviceReady();
-    }
+    app.title = 'Supplier Assessment';
 
-    function onDeviceReady() {
-        //>>excludeStart("build", true);
-        system.debug(true);
-        //>>excludeEnd("build");
+    app.configurePlugins({
+        router: true,
+        dialog: true
+    });
 
-        app.title = 'Supplier Assessment';
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
 
-        app.configurePlugins({
-            router: true,
-            dialog: true
-        });
-
-        app.start().then(function () {
-            //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-            //Look for partial views in a 'views' folder in the root.
-            viewLocator.useConvention();
-
-            //Show the app by setting the root view model for our application with a transition.
-            app.setRoot('viewmodels/shell', 'entrance');
-        });
-    };
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
 });
