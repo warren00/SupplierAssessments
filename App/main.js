c9e6100a9d327314aa3720ca3bde661aa3ffdc92
Steve Harper <steveharper886@gmail.com>
2014-12-19 13:43:12 +0000

    Fix

--- App/main.js
+++ App/main.js
@@ -15,7 +15,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
     system.debug(true);
     //>>excludeEnd("build");
 
-    app.title = 'VOW Supplier Assessment';
+    app.title = 'Supplier';
 
     app.configurePlugins({
         router: true,
