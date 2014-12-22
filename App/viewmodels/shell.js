daaf23f8cc2003e0587b680bf684830fcc9f9e58
Steve Harper <steveharper886@gmail.com>
2014-12-22 11:59:10 +0000

    Fix

--- App/viewmodels/shell.js
+++ App/viewmodels/shell.js
@@ -12,12 +12,6 @@
             navigateBack: navigateBack,
             attached: function () {
 
-                if (window.cordova != null && typeof (StatusBar) != "undefined") {
-                    StatusBar.overlaysWebView(false);
-                    StatusBar.styleDefault();
-                    StatusBar.show();
-                }
-
                 $(document).on('click', '.navbar-collapse.in', function (e) {
                     if ($(e.target).is('a')) {
                         $(this).collapse('hide');
