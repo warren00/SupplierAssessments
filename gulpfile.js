15ca9654828784d457629f55f8e202a8de399759
Steve Harper <steveharper886@gmail.com>
2015-01-06 14:38:31 +0000

    Fix

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+var durandal = require('gulp-durandal');
+
+gulp.task('default', function () {
+    return durandal({
+        baseDir: 'app',
+        main: 'main.js',
+        output: 'main-release.js',
+        almond: true,
+        minify: true
+    }).pipe(gulp.dest('App'));
+});
