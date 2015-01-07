8c174ff9f765a579a86347ae75de92b7e2e2699b
Steve Harper <steveharper886@gmail.com>
2015-01-07 15:10:29 +0000

    Fix

--- App/main.js
+++ App/main.js
@@ -20,8 +20,6 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (sy
             StatusBar.overlaysWebView(true);
             StatusBar.styleLightContent();
             StatusBar.show();
-
-            cordova.plugins.Keyboard.disableScroll(true);
         }
 
         //>>excludeStart("build", true);
