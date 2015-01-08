6ab4236fdbeb3d8afb467bb5ddbe913d8fd2c57e
Steve Harper <steveharper886@gmail.com>
2015-01-08 16:34:22 +0000

    Fix

--- App/viewmodels/searchSuppliers.js
+++ App/viewmodels/searchSuppliers.js
@@ -167,6 +167,19 @@
 
             pollForViewContainerWidthUpdate(view);
 
+            $(document).ready(function () {
+                $(document).on('keyup keypress', 'form input[type="text"]', function (e) {
+
+                    if (e.keyCode == 13) {
+                        e.preventDefault();
+
+                        $('input:focus').blur();
+
+                        return false;
+                    }
+                });
+            });
+
             $(window).scroll(function () {
                 var newSuppliers = ko.observableArray();
 
