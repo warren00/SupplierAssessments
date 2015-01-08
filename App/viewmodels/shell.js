d2b40131031afccbebc312ef3dfb2de533ed4aae
Steve Harper <steveharper886@gmail.com>
2015-01-08 12:18:55 +0000

    Fix

--- App/viewmodels/shell.js
+++ App/viewmodels/shell.js
@@ -12,6 +12,44 @@
             navigateBack: navigateBack,
             attached: function () {
 
+                var isFocused, focusedResizing;
+
+                var inputs = document.getElementsByTagName('input');
+                for (var i = 0; i < inputs.length; i++) {
+                    input = inputs[i];
+                    $(input).focus(focused);
+                    $(input).blur(blured);
+                }
+                window.onscroll = scrolled;
+
+                function focused(event) {
+                    isFocused = true;
+                    scrolled();
+                }
+
+                function blured(event) {
+                    isFocused = false;
+
+                    if (focusedResizing) {
+                        focusedResizing = false;
+                        $('.navbar').css('position', 'fixed');
+                        $('.navbar').css('top', '0');
+                    }
+                }
+
+                function scrolled() {
+                    if (isFocused) {
+                        if (!focusedResizing) {
+                            focusedResizing = true;
+                            $('.navbar').css("position", "absolute");
+                        }
+                        $('.navbar').css("top", window.pageYOffset + 'px');
+                        // window.innerHeight wrong
+                        //var footTop = window.pageYOffset + window.innerHeight - foot.offsetHeight;
+                        //footStyle.bottom = (document.body.offsetHeight - footTop) + 'px';
+                    }
+                }
+
                 if (device.platform === 'iOS' && parseFloat(device.version) >= 7.0) {
                     $(".navbar").css("border", "none")
                     $(".navbar-toggle").css("margin-right", "5px");
