a128af3eb1afe1f1f59b5a95fda4bcecc3a45500
Steve Harper <steveharper886@gmail.com>
2015-01-08 11:39:56 +0000

    Fix

--- App/viewmodels/shell.js
+++ App/viewmodels/shell.js
@@ -14,6 +14,7 @@
 
                 if (device.platform === 'iOS' && parseFloat(device.version) >= 7.0) {
                     $(".navbar").css("border", "none")
+                    $(".navbar-toggle").css("margin-right", "5px");
 
                     StatusBar.overlaysWebView(false);
                     StatusBar.backgroundColorByHexString("#474D54");
