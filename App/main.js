363ed61247ffaf7fb10addd3f16adeeb7e854292
Steve Harper <steveharper886@gmail.com>
2015-02-04 16:30:30 +0000

    Fix

--- App/main.js
+++ App/main.js
@@ -10,32 +10,62 @@
 define('jquery', function () { return jQuery; });
 define('knockout', ko);
 
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (system, app, viewLocator) {
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'platform'], function (system, app, viewLocator, platform) {
-    //>>excludeStart("build", true);
-    system.debug(false);
-    //>>excludeEnd("build");
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-    system.error = function (e) {
+    function operationFailed() {
 
-        platform.error();
+        var dialogTitle = "VOW Supplier Portal";
+        var noConnectionMessage = "No internet connection. Please check your connection and try again.";
+        var errorMessage = "Oops! There appears to be a problem with your application. Please close down and try again";
 
-        throw e;
-    };
+        if (window.cordova != null) {
+            var networkState = navigator.connection.type;
 
-    app.title = 'Supplier';
+            if (networkState == Connection.NONE) {
+                window.navigator.notification.alert(noConnectionMessage, null, dialogTitle, "Ok");
+                document.location = "login.html"
+            }
+            else {
+                window.navigator.notification.alert(errorMessage, null, dialogTitle, "Ok");
+            }
+        }
+        else {
+            alert(errorMessage);
+        }
+    }
 
-    app.configurePlugins({
-        router: true,
-        dialog: true
-    });
+    function onDeviceReady() {
+        StatusBar.overlaysWebView(false);
+        StatusBar.styleLightContent();
+        StatusBar.backgroundColorByHexString("#272b30");
+        StatusBar.show();
 
-    app.start().then(function () {
-        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-        //Look for partial views in a 'views' folder in the root.
-        viewLocator.useConvention();
+        //>>excludeStart("build", true);
+        system.debug(false);
+        //>>excludeEnd("build");
 
-        //Show the app by setting the root view model for our application with a transition.
-        app.setRoot('viewmodels/shell', 'entrance');
-    });
+        system.error = function (e) {
+            operationFailed();
+
+            throw e;
+        };
+
+        app.title = 'Supplier';
+
+        app.configurePlugins({
+            router: true,
+            dialog: true
+        });
+
+        app.start().then(function () {
+            //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+            //Look for partial views in a 'views' folder in the root.
+            viewLocator.useConvention();
+
+            //Show the app by setting the root view model for our application with a transition.
+            app.setRoot('viewmodels/shell', 'entrance');
+        });
+    }
 });
