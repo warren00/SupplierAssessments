c68123bc283ad11fa71e00e9e1ca5c6d69a0480f
Steve Harper <steveharper886@gmail.com>
2015-02-05 15:18:50 +0000

    Fix

--- App/config.js
+++ App/config.js
@@ -1,7 +1,7 @@
-﻿define(function () {
+﻿define(["platform"], function (platform) {
 
-    var remoteDataServicePath = "http://supplierassessment6729.azurewebsites.net/api/breeze"
-    var remoteAccountServicePath = "http://supplierassessment6729.azurewebsites.net/api/account";
+    var remoteDataServicePath = "api/breeze"
+    var remoteAccountServicePath = "api/account";
 
     var accountNumber = null;
 
@@ -23,9 +23,15 @@
                                { route: "assessments", moduleId: "viewmodels/assessments", name: "Assessments", nav: false },
                                { route: "assessment/:id*details", moduleId: "viewmodels/assessmentDetail", title: "Assessment Detail", hash: "#assessment/:id", nav: false },
                                { route: "deliveryAssessment/:id*details", moduleId: "viewmodels/deliveryAssessment", title: "Delivery Assessment", nav: false }];
-
     $.ajaxSetup({
-        timeout: 60000
+        timeout: 60000,
+        error: function (jqXHR, textStatus, errorThrown) {
+            if (jqXHR.status == 403) {
+                platform.logout();
+            } else {
+                platform.showMessage("Error: " + textStatus + ": " + errorThrown);
+            }
+        }
     });
 
     return {
